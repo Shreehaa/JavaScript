fe16cde41d7027833c7f6070e30ea306dd0eb374
Shreehaa_Balasubramani <115361905+Shreehaa@users.noreply.github.com>
2024-01-24 09:14:19 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,25 @@
+var a=[10,20,30]
+console.log(typeof a)
+console.log(a)
+var a =10
+console.log(typeof a)
+console.log(a)
+//way 1
+var a = { "class1 kec" : "ECE","class2 kec" : "CSD"}
+console.log(a)
+a["hall"]="seminar hall"
+console.log(a)
+//way 2
+var cust={}
+cust["id"]="21cdr047"
+cust["nme"]="shreehaa"
+cust["age"]="30"
+console.log(cust)
+//way 3
+var kongu = new Object()
+kongu["fees"]=100000
+kongu["food"]="good"
+kongu["hstlcount"]=10
+console.log(kongu)
+console.log(kongu.food)
+console.log(kongu["food"])
